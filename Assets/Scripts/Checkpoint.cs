61173644900120179f949a88a462615b747f0908
Jon Lynch <jonnylynchy@gmail.com>
2020-02-26 09:52:17 -0600

    added initial project files

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Sets the respawn location of a player when trigger
+public class Checkpoint : MonoBehaviour {
+
+	// Update is called once per frame
+	[System.Obsolete]
+	void OnTriggerEnter(Collider collision)						// used for things like bullets, which are triggers.  
+	{
+		if ((collision.gameObject.tag == "Player") && (collision.gameObject.GetComponent<Health> () != null))
+		{
+			collision.gameObject.GetComponent<Health>().updateRespawn(collision.gameObject.transform.position, collision.gameObject.transform.rotation);
+		}
+	}
+}
